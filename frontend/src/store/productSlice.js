1543800a7662be80a6bd2c2326a8cb2c0366552e
Asia Ashraf <asiaashraf7272@gmail.com>
2025-06-29 10:30:36 +0500

    update product slice

--- frontend/src/store/productSlice.js
+++ frontend/src/store/productSlice.js
@@ -1,35 +1,26 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk, isPending, isRejected } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-export const addProduct = createAsyncThunk(
-  'products/addProduct',
-  async (productData, thunkAPI) => {
-    const response = await axios.post('api/products', productData);
-    return response.data;
-  }
-);
-export const fetchProducts = createAsyncThunk(
-  'products/fetchProducts',
-  async (_, thunkAPI) => {
-    const response = await axios.get('api/products');
-    return response.data;
-  }
-);
-export const editProduct = createAsyncThunk(
-  'products/editProduct',
-  async ({ id, updatedData }, thunkAPI) => {
-    const response = await axios.put(`api/products/${id}`, updatedData);
-    return response.data;
-  }
-);
+export const addProduct = createAsyncThunk('products/addProduct', async (productData) => {
+  const response = await axios.post('api/products', productData);
+  return response.data;
+});
 
-export const deleteProduct = createAsyncThunk(
-  'product/deleteProduct',
-  async (productId, thunkAPI) => {
-    const response = await axios.delete(`api/products/${productId}`);
-    return response.data;
-  }
-);
+export const fetchProducts = createAsyncThunk('products/fetchProducts', async () => {
+  const response = await axios.get('api/products');
+  return response.data;
+});
+
+export const editProduct = createAsyncThunk('products/editProduct', async ({ id, updatedData }) => {
+  const response = await axios.put(`api/products/${id}`, updatedData);
+  return response.data;
+});
+
+export const deleteProduct = createAsyncThunk('product/deleteProduct', async (productId) => {
+  const response = await axios.delete(`api/products/${productId}`);
+  return response.data;
+});
+
 const productSlice = createSlice({
   name: 'product',
   initialState: {
@@ -37,64 +28,41 @@ const productSlice = createSlice({
     data: [],
     error: null,
   },
+  reducers: {},
   extraReducers: (builder) => {
-    // add Products
+
     builder
-      .addCase(addProduct.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(addProduct.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
+      // add 
       .addCase(addProduct.fulfilled, (state, action) => {
         state.isLoading = false;
         state.data.unshift(action.payload);
       })
-
-      // Fetch products 
-      .addCase(fetchProducts.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(fetchProducts.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
+      //fetch
       .addCase(fetchProducts.fulfilled, (state, action) => {
         state.isLoading = false;
         state.data = action.payload.data;
+      })
+      //edit
+      .addCase(editProduct.fulfilled, (state) => {
+        state.isLoading = false;
 
       })
-      // Update Product
-      .addCase(editProduct.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(editProduct.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
-      .addCase(editProduct.fulfilled, (state, action) => {
-        state.isLoading = false;
-        // state.data = action.payload;
-      })
-      //Delete Product
-      .addCase(deleteProduct.pending, (state) => {
-        state.isLoading = true;
-        state.error = null;
-      })
-      .addCase(deleteProduct.rejected, (state, action) => {
-        state.isLoading = false;
-        state.error = action.error.message;
-      })
+      //delete
       .addCase(deleteProduct.fulfilled, (state, action) => {
         state.isLoading = false;
+
+      })
+      // pending for all
+      .addMatcher(isPending(addProduct, fetchProducts, editProduct, deleteProduct), (state) => {
+        state.isLoading = true;
         state.error = null;
       })
+      // rejected  for all
+      .addMatcher(isRejected(addProduct, fetchProducts, editProduct, deleteProduct), (state, action) => {
+        state.isLoading = false;
+        state.error = action.error.message;
+      });
   }
-
 });
 
 export default productSlice.reducer;
